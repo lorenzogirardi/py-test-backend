6cdbab8a0706f9c152cab5d697ddbcfeb845c294
lorenzo <lorenzo@ethzero.it>
2020-10-27 23:47:19 +0100

    changed a bit the code

--- app.py
+++ app.py
@@ -1,27 +1,96 @@
-from flask import Flask
-from flask import jsonify
+#!flask/bin/python
+from flask import Flask, jsonify, abort, request, make_response, url_for, render_template
+
+app = Flask(__name__, static_url_path = "")
 
 
-app = Flask(__name__)
+    
+@app.errorhandler(400)
+def not_found(error):
+    return make_response(jsonify( { 'error': 'Bad request' } ), 400)
+
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify( { 'error': 'Not found' } ), 404)
+
+context = [
+    {
+        'id': 1,
+        'title': u'Cento 6',
+        'description': u'SystemV based', 
+        'done': False
+    },
+    {
+        'id': 2,
+        'title': u'Centos 7',
+        'description': u'SystemD based', 
+        'done': False
+    }
+]
 
 @app.route('/')
-def hello():
-    return "main path"
+def index():
+    return render_template('index.html')
 
-@app.route('/context1', methods=['GET', 'POST'])
-def c1():
-    return jsonify({'name':'context',
-                    'number':'1'})
+def make_public_task(task):
+    new_task = {}
+    for field in task:
+        if field == 'id':
+            new_task['uri'] = url_for('get_task', task_id = task['id'], _external = True)
+        else:
+            new_task[field] = task[field]
+    return new_task
+    
+@app.route('/api/v1.0/context', methods = ['GET'])
+def get_context():
+    return jsonify( { 'context': list(map(make_public_task, context)) } )
 
-@app.route('/context2', methods=['GET', 'POST'])
-def c2():
-    return jsonify({'name':'context',
-                    'number':'2'})
 
-@app.route('/context3', methods=['GET', 'POST'])
-def c3():
-    return jsonify({'name':'context',
-                    'number':'3'})
+@app.route('/api/v1.0/context/<int:task_id>', methods = ['GET'])
+def get_task(task_id):
+    task = list(filter(lambda t: t['id'] == task_id, context))
+    if len(task) == 0:
+        abort(404)
+    return jsonify( { 'task': make_public_task(task[0]) } )
 
+@app.route('/api/v1.0/context', methods = ['POST'])
+def create_task():
+    if not request.json or not 'title' in request.json:
+        abort(400)
+    task = {
+        'id': context[-1]['id'] + 1,
+        'title': request.json['title'],
+        'description': request.json.get('description', ""),
+        'done': False
+    }
+    context.append(task)
+    return jsonify( { 'task': make_public_task(task) } ), 201
+
+@app.route('/api/v1.0/context/<int:task_id>', methods = ['PUT'])
+def update_task(task_id):
+    task = list(filter(lambda t: t['id'] == task_id, context))
+    if len(task) == 0:
+        abort(404)
+    if not request.json:
+        abort(400)
+    if 'title' in request.json and type(request.json['title']) != unicode:
+        abort(400)
+    if 'description' in request.json and type(request.json['description']) is not unicode:
+        abort(400)
+    if 'done' in request.json and type(request.json['done']) is not bool:
+        abort(400)
+    task[0]['title'] = request.json.get('title', task[0]['title'])
+    task[0]['description'] = request.json.get('description', task[0]['description'])
+    task[0]['done'] = request.json.get('done', task[0]['done'])
+    return jsonify( { 'task': make_public_task(task[0]) } )
+    
+@app.route('/api/v1.0/context/<int:task_id>', methods = ['DELETE'])
+def delete_task(task_id):
+    task = list(filter(lambda t: t['id'] == task_id, context))
+    if len(task) == 0:
+        abort(404)
+    context.remove(task[0])
+    return jsonify( { 'result': True } )
+    
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0")
